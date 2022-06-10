88e2b2553b4d4fe9c4552d4701d54641054a27d0
Litvinov Iurii <litvinov.yuriy1996@gmail.com>
2022-06-11 02:42:19 +0400

    feat: Added file system manager

---
+++ src/command/fsManager.js
@@ -0,0 +1,69 @@
+import path from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+import { Printer } from '../printer.js';
+import { rename, unlink, open } from 'fs/promises';
+import { pipeline } from 'stream/promises';
+
+export class FileSystemManager {
+    static operations = {
+        cat: this.showFileContent.bind(this),
+        add: this.addNewFile.bind(this),
+        mv: this.moveFile.bind(this),
+        rn: this.renameFile.bind(this),
+        cp: this.copyFile.bind(this),
+        rm: this.deleteFile.bind(this),
+    }
+
+    static async run(cmd, currentDir) {
+        const [operator, ...args] = cmd.split(' ');
+        await this.operations[operator](args, currentDir);
+    }
+
+    static async showFileContent([pathToFile], currentDir) {
+        return new Promise((resolve, reject) => {
+            const stream = createReadStream(
+                path.resolve(currentDir, pathToFile), 
+                { encoding: 'utf8' }
+            );
+
+            stream.on('readable', () => {
+                    const data = stream.read();
+                    if (data) {
+                        Printer.print(data);
+                    }
+                })
+                .on('end', resolve)
+                .on('error', reject);
+        });
+    }
+
+    static async addNewFile([pathToFile], currentDir) {
+        await open(path.resolve(currentDir, pathToFile), 'ax');
+    }
+
+    static async renameFile([oldPath, newFileName], currentDir) {
+        const pathToFile = path.resolve(currentDir, oldPath);
+        await rename(
+            pathToFile,
+            path.resolve(pathToFile, '../', newFileName)
+        );
+    }
+
+    static async copyFile([oldPath, pathToNewDir], currentDir) {
+        const pathToFile = path.resolve(currentDir, oldPath);
+        const fileName = path.basename(pathToFile);
+        await pipeline(
+            createReadStream(pathToFile, { encoding: 'utf8' }),
+            createWriteStream(path.resolve(currentDir, pathToNewDir, fileName))
+        );
+    }
+
+    static async moveFile(...args) {
+        await this.copyFile(...args);
+        await this.deleteFile(...args);
+    }
+
+    static async deleteFile([pathToDeletedFile], currentDir) {
+        await unlink(path.resolve(currentDir, pathToDeletedFile));
+    }
+}
